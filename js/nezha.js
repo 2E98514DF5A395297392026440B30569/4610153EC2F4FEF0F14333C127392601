f0a5673f04b17cf64192fd3df101641b60c34148
2E98514DF5A395297392026440B30569 <118182097+2E98514DF5A395297392026440B30569@users.noreply.github.com>
2023-12-21 21:35:20 +0800

    Update nezha.js

--- js/nezha.js
+++ js/nezha.js
@@ -2,9 +2,9 @@
 window.onload = function(){
 var avatar=document.querySelector(".item img")
 var footer=document.querySelector("div.is-size-7")
-document.querySelector("[rel='shortcut icon']").href = "https://image.narutos.top/images/2023/11/06/128.png"
+document.querySelector("[rel='shortcut icon']").href = "https://img.narutos.top/images/2023/11/06/128.png"
 footer.innerHTML="うずまきナルトのクラウドサーバー"
 footer.style.visibility="visible"
-avatar.src="https://image.narutos.top/images/2023/11/06/128.png"
+avatar.src="https://img.narutos.top/images/2023/11/06/128.png"
 avatar.style.visibility="visible"
 }
